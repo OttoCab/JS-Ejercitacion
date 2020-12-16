3c355c4d01eeb2c8c9a1f24945c1a5302ce4950a
Oscar Cabanillas <oskbanillas@gmail.com>
2020-12-16 20:52:18 -0300

    Se creo la estructura del proyecto y se resolvio el ejercicio numero 1

---
+++ js/1-Ejercicios Condicionales/1-ejercicio.js
@@ -0,0 +1,10 @@
+alert("Un Mensaje");
+
+//Otra Forma de hacer lo mismo
+//window.alert("Un Mensaje");
+
+//Este es parecido solo que agrega la opcion "Aceptar" o "Cancelar"
+//confirm("Un Mensaje")
+
+//Con este surge una ventana que nos pide ingresar algo
+//prompt()
