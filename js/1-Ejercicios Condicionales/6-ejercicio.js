51eb08354214421606c88ad38844c2392753bbaa
Oscar Cabanillas <oskbanillas@gmail.com>
2020-12-16 22:09:29 -0300

    Se resolvio el problema 6

---
+++ js/1-Ejercicios Condicionales/6-ejercicio.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt("Ingrese numero a comparar"));
+let num2 = parseInt(prompt("Ingrese otro numero a comparar"));
+
+if (num1 > num2){
+    document.write("El numero " + num1 + " es mayor");
+}else{
+    document.write("El numero " + num2 + " es mayor")
+}
