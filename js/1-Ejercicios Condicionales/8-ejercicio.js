da0da9dc9edd9a8b9c3003d2064c2a2f343fb4b6
Oscar Cabanillas <oskbanillas@gmail.com>
2020-12-16 22:35:26 -0300

    Pequeña correccion del problema 8

---
+++ js/1-Ejercicios Condicionales/8-ejercicio.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(prompt("Ingrese ingresa un numero para saber si es divisible en 2"));
+let result = num1%2;
+
+
+if (result == 0) document.write("El numero es divisible en 2");
+else document.write("El numero no es divisible en 2"); 
