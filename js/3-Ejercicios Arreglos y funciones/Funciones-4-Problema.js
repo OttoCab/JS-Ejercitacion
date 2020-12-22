05b58c5f0a5ceccefaab5dee43d9fd9c32ce65cf
Oscar Cabanillas <oskbanillas@gmail.com>
2020-12-21 21:52:47 -0300

    Resuelto el problema 4 de Funciones

---
+++ js/3-Ejercicios Arreglos y funciones/Funciones-4-Problema.js
@@ -0,0 +1,18 @@
+/*4- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, 
+la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 
+del número elegido por el usuario.*/
+
+let valor = parseInt(prompt("Ingrese un numero para realizar su table de multiplicar"));
+let multiplicar;
+
+tablaMul(valor);
+
+function tablaMul(num) {
+    for (let i = 1; i <= 10 ; i++) {
+        multiplicar = num * i;
+        document.write(`${num}x${i}=${multiplicar}`); 
+        document.write(`<br>`);       
+    }
+     
+}
+
