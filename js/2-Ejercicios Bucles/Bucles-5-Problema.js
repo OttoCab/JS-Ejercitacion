6442ebc96bd17db130f3814a5a78a6330681e23d
Oscar Cabanillas <oskbanillas@gmail.com>
2021-01-30 19:35:48 -0300

    Resuelto el problema 5 de bucles

--- js/2-Ejercicios Bucles/Bucles-5-Problema.js
+++ js/2-Ejercicios Bucles/Bucles-5-Problema.js
@@ -11,99 +11,95 @@
 // *Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 
 let numDNI = prompt("Ingrese el numero de su DNI");
-let flag = true;
 
-while (flag) {
-  //Si el valor cargado es incorrecto, solicito que lo vuelva a ingresar
-  while (isNaN(numDNI)) {
-    alert("No se ingreso un valor valido");
-    numDNI = prompt("Ingrese el numero de su DNI");
-  }
-
-  //Compruebo que el el numero ingresado este en el rango especificado
+//Si el valor cargado es incorrecto, solicito que lo vuelva a ingresar
+while (isNaN(numDNI)) {
+  alert("No se ingreso un valor valido");
+  numDNI = prompt("Ingrese el numero de su DNI");
   while (numDNI < 0 || numDNI > 99999999) {
     alert("El numero de documento ingresado no es valido");
     numDNI = prompt("Ingrese de nuevo el numero de su DNI");
   }
-  flag=false;
 }
+
+//Compruebo que el el numero ingresado este en el rango especificado
 
 let resto = numDNI % 23;
 let letter = "";
 //letter (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E)
 switch (resto) {
-    case 0:
-    letter = "T"
-        break;
-    case 1:
-    letter = "R"
-        break;
-    case 2:
-    letter = "W"
-        break;
-    case 3:
-    letter = "A"
-        break;
-    case 4:
-    letter = "G"
-        break;
-    case 5:
-    letter = "M"
-        break;
-    case 6:
-    letter = "Y"
-        break;
-    case 7:
-    letter = "F"
-        break;
-    case 8:
-    letter = "P"
-        break;
-    case 9:
-    letter = "D"
-        break;
-    case 10:
-    letter = "X"
-        break;
-    case 11:
-    letter = "B"
-        break;
-    case 12:
-    letter = "N"
-        break;
-    case 13:
-    letter = "J"
-        break;
-    case 14:
-    letter = "Z"
-        break;
-    case 15:
-    letter = "S"
-        break;
-    case 16:
-    letter = "Q"
-        break;
-    case 17:
-    letter = "V"
-        break;
-    case 18:
-    letter = "H"
-        break;
-    case 19:
-    letter = "L"
-        break;
-    case 20:
-    letter = "C"
-        break;
-    case 21:
-    letter = "K"
-        break;
-    case 22:
-    letter = "E"
-        break;
+  case 0:
+    letter = "T";
+    break;
+  case 1:
+    letter = "R";
+    break;
+  case 2:
+    letter = "W";
+    break;
+  case 3:
+    letter = "A";
+    break;
+  case 4:
+    letter = "G";
+    break;
+  case 5:
+    letter = "M";
+    break;
+  case 6:
+    letter = "Y";
+    break;
+  case 7:
+    letter = "F";
+    break;
+  case 8:
+    letter = "P";
+    break;
+  case 9:
+    letter = "D";
+    break;
+  case 10:
+    letter = "X";
+    break;
+  case 11:
+    letter = "B";
+    break;
+  case 12:
+    letter = "N";
+    break;
+  case 13:
+    letter = "J";
+    break;
+  case 14:
+    letter = "Z";
+    break;
+  case 15:
+    letter = "S";
+    break;
+  case 16:
+    letter = "Q";
+    break;
+  case 17:
+    letter = "V";
+    break;
+  case 18:
+    letter = "H";
+    break;
+  case 19:
+    letter = "L";
+    break;
+  case 20:
+    letter = "C";
+    break;
+  case 21:
+    letter = "K";
+    break;
+  case 22:
+    letter = "E";
+    break;
 
-    default:
-        break;
+  default:
+    break;
 }
 
 document.write("El numero de DNI ingresado es: " + numDNI + "<br>");
