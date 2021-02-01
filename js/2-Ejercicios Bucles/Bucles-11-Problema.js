bb4c0fb80717eeea3d2f44d31bed27d2566a231e
Oscar Cabanillas <oskbanillas@gmail.com>
2021-02-01 16:35:34 -0300

    Realizado el problema 11 de Bucles

---
+++ js/2-Ejercicios Bucles/Bucles-11-Problema.js
@@ -0,0 +1,19 @@
+/*11- Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. *
+
+Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp */
+
+let usarName = [];
+let age = 0, bigNum = 0, pos = 0; 
+
+for (let i = 0; i < 3; i++) {
+    usarName.push(prompt("Ingrese un nombre: "))
+    age = prompt("Ingrese una edad: ")
+
+    if (age > bigNum) {
+        bigNum = age;
+        pos = i;        
+    }
+    
+}
+
+document.write(usarName[pos])
