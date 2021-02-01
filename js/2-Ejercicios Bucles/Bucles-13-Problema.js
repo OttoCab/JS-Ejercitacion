f235d92d6be50ec3c931523c54c9167aa4ee7aa7
Oscar Cabanillas <oskbanillas@gmail.com>
2021-02-01 17:09:13 -0300

    Resuleto el problema 13 de Bucles

---
+++ js/2-Ejercicios Bucles/Bucles-13-Problema.js
@@ -0,0 +1,5 @@
+/* 13- Realiza un script que pida un texto y lo muestre en mayúsculas. */
+
+let chains = prompt("Ingrese un texto para mostrarlo en mayuscula");
+
+document.write(chains.toUpperCase());
