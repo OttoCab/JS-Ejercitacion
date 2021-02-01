bd454e4cc274bc6147903a9a422f342d383327be
Oscar Cabanillas <oskbanillas@gmail.com>
2021-02-01 17:03:57 -0300

    Pequeña correcion en el problema 11 de Bucles

--- js/2-Ejercicios Bucles/Bucles-11-Problema.js
+++ js/2-Ejercicios Bucles/Bucles-11-Problema.js
@@ -2,18 +2,42 @@
 
 Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp */
 
-let usarName = [];
-let age = 0, bigNum = 0, pos = 0; 
+//FORMA 1
+
+let usarName = [], age = [];
+let bigNum = 0, pos = 0;
 
 for (let i = 0; i < 3; i++) {
     usarName.push(prompt("Ingrese un nombre: "))
-    age = prompt("Ingrese una edad: ")
+    age.push(prompt("Ingrese una edad: "))
+}
 
-    if (age > bigNum) {
-        bigNum = age;
-        pos = i;        
+bigNum = Math.max(age[0], age[1], age[2]);
+
+for (let i = 0; i < 3; i++) {
+    if (bigNum == age[i]) {        
+        pos = i;
     }
-    
+
 }
 
 document.write(usarName[pos])
+
+
+//FORMA 2
+
+// let usarName = [];
+// let age = 0, bigNum = 0, pos = 0; 
+
+// for (let i = 0; i < 3; i++) {
+//     usarName.push(prompt("Ingrese un nombre: "))
+//     age = prompt("Ingrese una edad: ")
+
+//     if (age > bigNum) {
+//         bigNum = age;
+//         pos = i;        
+//     }
+
+// }
+
+// document.write(usarName[pos])
